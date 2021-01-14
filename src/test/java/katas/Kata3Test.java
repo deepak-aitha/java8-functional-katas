72e1cba31b5f6c85ab05340bc4d8cc8a0ab85d47
Pietro Di Bello <pierodibello@gmail.com>
2021-01-14 23:12:32 +0100

    add a more specific test for Kata3

--- src/test/java/katas/Kata3Test.java
+++ src/test/java/katas/Kata3Test.java
@@ -2,14 +2,19 @@ package katas;
 
 import org.junit.Test;
 
-import static org.hamcrest.CoreMatchers.equalTo;
-import static org.junit.Assert.assertThat;
+import java.util.List;
 
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.Matchers.containsInAnyOrder;
+import static org.junit.Assert.assertThat;
 
 public class Kata3Test {
 
     @Test
     public void testExecute() {
-        assertThat(Kata3.execute().size(), equalTo(4));
+        List<Integer> ids = Kata3.execute();
+
+        assertThat(ids.size(), equalTo(4));
+        assertThat(ids, containsInAnyOrder(70111470, 654356453, 65432445, 675465));
     }
 }
