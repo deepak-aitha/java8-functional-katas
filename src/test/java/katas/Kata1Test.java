24845aec60fa99fada15d91d326d97c675c19132
Timoty Granziero <timoty.granziero@arcavita.it>
2021-01-14 17:57:57 +0100

    Integrate Kata1Test

--- src/test/java/katas/Kata1Test.java
+++ src/test/java/katas/Kata1Test.java
@@ -2,6 +2,9 @@ package katas;
 
 import org.junit.Test;
 
+import java.util.List;
+import java.util.Map;
+
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.junit.Assert.assertThat;
 
@@ -10,6 +13,19 @@ public class Kata1Test {
 
     @Test
     public void testExecute() {
-        assertThat(Kata1.execute().size(), equalTo(4));
+        List<Map<String, ?>> result = Kata1.execute();
+        assertThat(result.size(), equalTo(4));
+        assertThat(result.get(0).size(), equalTo(2));
+        assertThat(result.get(0).get("id"), equalTo(70111470));
+        assertThat(result.get(0).get("title"), equalTo("Die Hard"));
+        assertThat(result.get(1).size(), equalTo(2));
+        assertThat(result.get(1).get("id"), equalTo(654356453));
+        assertThat(result.get(1).get("title"), equalTo("Bad Boys"));
+        assertThat(result.get(2).size(), equalTo(2));
+        assertThat(result.get(2).get("id"), equalTo(65432445));
+        assertThat(result.get(2).get("title"), equalTo("The Chamber"));
+        assertThat(result.get(3).size(), equalTo(2));
+        assertThat(result.get(3).get("id"), equalTo(675465));
+        assertThat(result.get(3).get("title"), equalTo("Fracture"));
     }
 }
