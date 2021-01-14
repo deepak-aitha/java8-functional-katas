32cc0e53a836c3ccb9a6a18e6f2f95d2c3c8a4b0
Pietro Di Bello <pierodibello@gmail.com>
2021-01-14 22:54:56 +0100

    use Arrays.asList as it's more popular and easily understandable

--- src/main/java/katas/Kata2.java
+++ src/main/java/katas/Kata2.java
@@ -1,12 +1,10 @@
 package katas;
 
-import com.google.common.collect.ImmutableList;
-
-import java.util.List;
-import java.util.stream.Collectors;
-
 import model.Movie;
 import util.DataUtil;
+
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * Goal: Chain filter() and map() to collect the ids of videos that have a rating of 5.0
@@ -17,6 +15,6 @@ public class Kata2 {
     public static List<Integer> execute() {
         List<Movie> movies = DataUtil.getMovies();
 
-        return ImmutableList.of(1, 2, 3);
+        return Arrays.asList(1, 2, 3);
     }
 }
